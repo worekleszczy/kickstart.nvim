43b7de6b7ae3a93c6f10904891853abf45e20cbf
Piotr Josiak <piotr.josiak@bluelabs.eu>
2023-09-22 14:25:48 +0200

    feat: Add telescope grep args

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,16 @@
 return {
-  "f-person/git-blame.nvim",
-  config = function()
-    vim.cmd [[silent! GitBlameDisable]]
-  end
+  {
+    "f-person/git-blame.nvim",
+    config = function()
+      vim.cmd [[silent! GitBlameDisable]]
+    end
+  },
+  {
+    "sindrets/diffview.nvim",
+    setup = true,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    setup = true
+  }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,17 +3,44 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     version = '*',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      defaults = {
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        version = "^1.0.0",
+      }
+    },
+    config = function()
+      local telescope = require("telescope")
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+            },
           },
         },
-      },
-    },
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              },
+            },
+            -- ... also accepts theme settings, for example:
+            -- theme = "dropdown", -- use dropdown theme
+            -- theme = { }, -- use own theme spec
+            -- layout_config = { mirror=true }, -- mirror preview pane
+          }
+        }
+      })
+      telescope.load_extension("live_grep_args")
+    end
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
@@ -75,7 +102,7 @@ return {
       },
       {
         '<leader>sg',
-        function() require('telescope.builtin').live_grep() end,
+        function() require('telescope').extensions.live_grep_args.live_grep_args() end,
         desc =
         '[s]earch by [g]rep'
       },
